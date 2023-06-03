ea0ba856b18e5ee0d34e90ff9f1846cf0939e70a
Korede Faleye <akfaleye@gmail.com>
2023-06-03 01:27:40 +0100

    javascript/eloquent_js_exercises: Solved chapter 4 Question 3

---
+++ javascript/eloquent_js_exercises/chapter_4/4.3_a_list.js
@@ -0,0 +1,56 @@
+/* 
+ * let list = {
+    value: 1,
+    rest: {
+      value: 2,
+      rest: {
+        value: 3,
+        rest: null
+      }
+    }
+  };
+ Write a function arrayToList that builds up a list structure like the one shown when given [1, 2, 3] as argument. 
+ 
+ Also write a listToArray function that produces an array from a list. 
+ 
+ Then add a helper function prepend, which takes an element and a list and creates a new list that adds the element to the front of the input list, and nth, which takes a list and a number and returns the element at the given position in the list (with zero referring to the first element) or undefined when there is no such element.
+
+  If you haven’t already, also write a recursive version of nth.
+  *
+  */
+
+/* Authors Solution */
+function arrayToList(array) {
+  let list = null;
+  for (let i = array.length - 1; i >= 0; i--) {
+    list = {value: array[i], rest: list};
+  }
+  return list;
+}
+
+function listToArray(list) {
+  let array = [];
+  for (let node = list; node; node = node.rest) {
+    array.push(node.value);
+  }
+  return array;
+}
+
+function prepend(value, list) {
+  return {value, rest: list};
+}
+
+function nth(list, n) {
+  if (!list) return undefined;
+  else if (n == 0) return list.value;
+  else return nth(list.rest, n - 1);
+}
+
+console.log(arrayToList([10, 20,30,40,50]));
+// → {value: 10, rest: {value: 20, rest: null}}
+//console.log(listToArray(arrayToList([10, 20, 30])));
+// → [10, 20, 30]
+//console.log(prepend(10, prepend(20, null)));
+// → {value: 10, rest: {value: 20, rest: null}}
+//console.log(nth(arrayToList([10, 20, 30]), 1));
+// → 20
